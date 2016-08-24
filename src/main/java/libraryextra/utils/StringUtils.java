61c07dd541e85cff8298afe7f0f2181f49622c34
139634zgc <13001253655@163.com>
2016-08-24 11:30:04 +0800

    StringUtils 方法添加

--- src/main/java/libraryextra/utils/StringUtils.java
+++ src/main/java/libraryextra/utils/StringUtils.java
@@ -9,6 +9,8 @@ import android.text.style.AbsoluteSizeSpan;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringUtils {
 
@@ -18,6 +20,39 @@ public class StringUtils {
      * @param strChinese
      * @return
      */
+    /****
+     * 是否是有效电话
+     ****/
+    public static boolean isPhone(String name) {
+        Pattern p = Pattern.compile("^((13[0-9])|(17[0-9])|(14[0-9])|(15[^4,\\D])|(18[0-9]))\\d{8}$");
+        Matcher m = p.matcher(name);
+        return m.matches();
+    }
+
+    /***
+     * 是否是有效Email
+     ***/
+    public static boolean isEmail(String email) {
+        boolean flag = true;
+        if (email.indexOf("@") == -1 || email.indexOf(".") == -1) {
+
+            flag = false;
+        }
+        if (flag) {
+            if (email.indexOf("@") > email.indexOf("."))
+                flag = false;
+        }
+        return flag;
+    }
+
+    /**
+     * 是否是有效密码
+     ***/
+    public static boolean isGoodPWD(String pwd) {
+        Pattern p = Pattern.compile("^[a-zA-Z0-9_]{6,16}$");
+        Matcher m = p.matcher(pwd);
+        return m.matches();
+    }
 
     public static String getPYIndexStr(String strChinese) {
         if (strChinese.equals("阚")) {
