6091679861c3a599fdf9223874b72bb40427745c
139634zgc <13001253655@163.com>
2016-09-01 10:27:42 +0800

    订单详情bean

---
+++ src/main/java/libraryextra/bean/OrderDetailBean.java
@@ -0,0 +1,20 @@
+package libraryextra.bean;
+
+import java.io.Serializable;
+
+/**
+
+ * @date 2016/8/31 15:27
+ * @Description 跳转到订单详情页面所需的bean
+ */
+public class OrderDetailBean implements Serializable {
+    public int id;
+    public String image;
+    public String name;
+    public String subject;
+    public String grade;
+    public int Completed_lesson_count;
+    public int Preset_lesson_count;
+    public String teacher;
+    public int price;
+}
