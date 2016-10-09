0bac06ab2463cce5abcb16dc3177c265f4b8ef86
TianhaoranLY <tianhaoran@bjwwtd.com>
2016-10-09 15:02:55 +0800

    QTA-53 充值记录条目点击事件

---
+++ src/main/java/libraryextra/bean/AppPayParamsBean.java
@@ -0,0 +1,72 @@
+package libraryextra.bean;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public  class AppPayParamsBean implements Serializable {
+            private String appid;
+            private String partnerid;
+            @SerializedName("package")
+            private String packageX;
+            private String timestamp;
+            private String prepayid;
+            private String noncestr;
+            private String sign;
+
+            public String getAppid() {
+                return appid;
+            }
+
+            public void setAppid(String appid) {
+                this.appid = appid;
+            }
+
+            public String getPartnerid() {
+                return partnerid;
+            }
+
+            public void setPartnerid(String partnerid) {
+                this.partnerid = partnerid;
+            }
+
+            public String getPackage() {
+                return packageX;
+            }
+
+            public void setPackage(String packageX) {
+                this.packageX = packageX;
+            }
+
+            public String getTimestamp() {
+                return timestamp;
+            }
+
+            public void setTimestamp(String timestamp) {
+                this.timestamp = timestamp;
+            }
+
+            public String getPrepayid() {
+                return prepayid;
+            }
+
+            public void setPrepayid(String prepayid) {
+                this.prepayid = prepayid;
+            }
+
+            public String getNoncestr() {
+                return noncestr;
+            }
+
+            public void setNoncestr(String noncestr) {
+                this.noncestr = noncestr;
+            }
+
+            public String getSign() {
+                return sign;
+            }
+
+            public void setSign(String sign) {
+                this.sign = sign;
+            }
+        }
