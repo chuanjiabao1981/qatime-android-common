d856207792600de0409c479c45214a29db361f46
TianhaoranLY <tianhaoran@bjwwtd.com>
2017-02-16 10:46:54 +0800

    QTA-237 选中处理

--- src/main/java/libraryextra/bean/ImageBucket.java
+++ src/main/java/libraryextra/bean/ImageBucket.java
@@ -10,8 +10,17 @@ public class ImageBucket {
 	public int count = 0;
 	public String bucketName;
 	public List<ImageItem> imageList;
+	private boolean isSelected;
 
 	public String getFirstImagePath() {
 		return imageList.get(0).imagePath;
+	}
+
+	public void setIsSelected(boolean isSelected) {
+		this.isSelected = isSelected;
+	}
+
+	public boolean isSelected() {
+		return isSelected;
 	}
 }
