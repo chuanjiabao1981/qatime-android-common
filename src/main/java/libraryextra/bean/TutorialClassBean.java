601ea627f39104fe80fb56c4d9db2c958d4460c3
lungtify <15617685965@163.com>
2017-03-23 16:48:05 +0800

    我的辅导数据

--- src/main/java/libraryextra/bean/TutorialClassBean.java
+++ src/main/java/libraryextra/bean/TutorialClassBean.java
@@ -9,47 +9,61 @@ import java.util.List;
  * @Description
  */
 public class TutorialClassBean implements Serializable {
-    private int status;
 
+    /**
+     * status : 1
+     * data : [{"id":61,"name":"再来一个辅导班","subject":"化学","grade":"高二","teacher_name":"王志成","price":10,"current_price":10,"chat_team_id":"25194018","chat_team_owner":"07b7c43a854ed44d36c2941f1fc5ad00","buy_tickets_count":4,"status":"published","preset_lesson_count":1,"completed_lesson_count":0,"taste_count":0,"completed_lessons_count":0,"closed_lessons_count":0,"live_start_time":"2016-11-22 09:00","live_end_time":"2016-11-22 10:00","objective":null,"suit_crowd":null,"publicize":"http://qatime-testing.oss-cn-beijing.aliyuncs.com/courses/publicize/list_b876ab65a61aaa1ae47597ef144b9f45.png","pull_address":"rtmp://va0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419","board_pull_stream":"rtmp://va0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419","camera_pull_stream":"rtmp://va0a19f55.live.126.net/live/0ca7943afaa340c9a7c1a8baa5afac97","preview_time":"2016-11-22 09:00","is_tasting":true,"is_bought":false,"tasted":false}]
+     */
+
+    private int status;
     private List<Data> data;
+
+    public int getStatus() {
+        return status;
+    }
 
     public void setStatus(int status) {
         this.status = status;
     }
 
-    public int getStatus() {
-        return this.status;
+    public List<Data> getData() {
+        return data;
     }
 
     public void setData(List<Data> data) {
         this.data = data;
     }
 
-    public List<Data> getData() {
-        return this.data;
-    }
-
-    public class Data implements Serializable {
-
+    public static class Data {
         /**
-         * id : 5
-         * name : 高中数学一轮复习第一章集合第二课时
-         * subject : 数学
-         * grade : 高三
-         * teacher_name : 姜海全
-         * price : 0
-         * chat_team_id : 7964470
-         * buy_tickets_count : 1
+         * id : 61
+         * name : 再来一个辅导班
+         * subject : 化学
+         * grade : 高二
+         * teacher_name : 王志成
+         * price : 10
+         * current_price : 10
+         * chat_team_id : 25194018
+         * chat_team_owner : 07b7c43a854ed44d36c2941f1fc5ad00
+         * buy_tickets_count : 4
+         * status : published
          * preset_lesson_count : 1
          * completed_lesson_count : 0
-         * live_start_time : 2016-08-10 4:00
-         * live_end_time : 2016-08-10 18:30
-         * publicize : http://qatime-testing.oss-cn-beijing.aliyuncs.com/courses/publicize/list_929d05eabae8e06550620885ecc115f7.jpg
-         * board : rtmp://va0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419
-         * camera :
-         * preview_time : 2016-08-10 18:00
+         * taste_count : 0
+         * completed_lessons_count : 0
+         * closed_lessons_count : 0
+         * live_start_time : 2016-11-22 09:00
+         * live_end_time : 2016-11-22 10:00
+         * objective : null
+         * suit_crowd : null
+         * publicize : http://qatime-testing.oss-cn-beijing.aliyuncs.com/courses/publicize/list_b876ab65a61aaa1ae47597ef144b9f45.png
+         * pull_address : rtmp://va0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419
+         * board_pull_stream : rtmp://va0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419
+         * camera_pull_stream : rtmp://va0a19f55.live.126.net/live/0ca7943afaa340c9a7c1a8baa5afac97
+         * preview_time : 2016-11-22 09:00
          * is_tasting : true
          * is_bought : false
+         * tasted : false
          */
 
         private int id;
@@ -57,39 +71,29 @@ public class TutorialClassBean implements Serializable {
         private String subject;
         private String grade;
         private String teacher_name;
-        public float price;
-        public float current_price;
+        private int price;
+        private double current_price;
         private String chat_team_id;
         private String chat_team_owner;
         private int buy_tickets_count;
+        private String status;
         private int preset_lesson_count;
         private int completed_lesson_count;
+        private int taste_count;
+        private int completed_lessons_count;
+        private int closed_lessons_count;
         private String live_start_time;
         private String live_end_time;
+        private Object objective;
+        private Object suit_crowd;
         private String publicize;
+        private String pull_address;
         private String board_pull_stream;
         private String camera_pull_stream;
         private String preview_time;
         private boolean is_tasting;
         private boolean is_bought;
-        private String status;
         private boolean tasted;
-
-        public boolean isTasted() {
-            return tasted;
-        }
-
-        public void setTasted(boolean tasted) {
-            this.tasted = tasted;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-
-        public void setStatus(String status) {
-            this.status = status;
-        }
 
         public int getId() {
             return id;
@@ -131,19 +135,19 @@ public class TutorialClassBean implements Serializable {
             this.teacher_name = teacher_name;
         }
 
-        public float getPrice() {
+        public int getPrice() {
             return price;
         }
 
-        public void setPrice(float price) {
+        public void setPrice(int price) {
             this.price = price;
         }
 
-        public float getCurrent_price() {
+        public double getCurrent_price() {
             return current_price;
         }
 
-        public void setCurrent_price(float current_price) {
+        public void setCurrent_price(double current_price) {
             this.current_price = current_price;
         }
 
@@ -171,6 +175,14 @@ public class TutorialClassBean implements Serializable {
             this.buy_tickets_count = buy_tickets_count;
         }
 
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
         public int getPreset_lesson_count() {
             return preset_lesson_count;
         }
@@ -185,6 +197,30 @@ public class TutorialClassBean implements Serializable {
 
         public void setCompleted_lesson_count(int completed_lesson_count) {
             this.completed_lesson_count = completed_lesson_count;
+        }
+
+        public int getTaste_count() {
+            return taste_count;
+        }
+
+        public void setTaste_count(int taste_count) {
+            this.taste_count = taste_count;
+        }
+
+        public int getCompleted_lessons_count() {
+            return completed_lessons_count;
+        }
+
+        public void setCompleted_lessons_count(int completed_lessons_count) {
+            this.completed_lessons_count = completed_lessons_count;
+        }
+
+        public int getClosed_lessons_count() {
+            return closed_lessons_count;
+        }
+
+        public void setClosed_lessons_count(int closed_lessons_count) {
+            this.closed_lessons_count = closed_lessons_count;
         }
 
         public String getLive_start_time() {
@@ -203,6 +239,22 @@ public class TutorialClassBean implements Serializable {
             this.live_end_time = live_end_time;
         }
 
+        public Object getObjective() {
+            return objective;
+        }
+
+        public void setObjective(Object objective) {
+            this.objective = objective;
+        }
+
+        public Object getSuit_crowd() {
+            return suit_crowd;
+        }
+
+        public void setSuit_crowd(Object suit_crowd) {
+            this.suit_crowd = suit_crowd;
+        }
+
         public String getPublicize() {
             return publicize;
         }
@@ -211,20 +263,28 @@ public class TutorialClassBean implements Serializable {
             this.publicize = publicize;
         }
 
-        public String getBoard() {
+        public String getPull_address() {
+            return pull_address;
+        }
+
+        public void setPull_address(String pull_address) {
+            this.pull_address = pull_address;
+        }
+
+        public String getBoard_pull_stream() {
             return board_pull_stream;
         }
 
-        public void setBoard(String board) {
-            this.board_pull_stream = board;
+        public void setBoard_pull_stream(String board_pull_stream) {
+            this.board_pull_stream = board_pull_stream;
         }
 
-        public String getCamera() {
+        public String getCamera_pull_stream() {
             return camera_pull_stream;
         }
 
-        public void setCamera(String camera) {
-            this.camera_pull_stream = camera;
+        public void setCamera_pull_stream(String camera_pull_stream) {
+            this.camera_pull_stream = camera_pull_stream;
         }
 
         public String getPreview_time() {
@@ -250,6 +310,13 @@ public class TutorialClassBean implements Serializable {
         public void setIs_bought(boolean is_bought) {
             this.is_bought = is_bought;
         }
-    }
 
+        public boolean isTasted() {
+            return tasted;
+        }
+
+        public void setTasted(boolean tasted) {
+            this.tasted = tasted;
+        }
+    }
 }
