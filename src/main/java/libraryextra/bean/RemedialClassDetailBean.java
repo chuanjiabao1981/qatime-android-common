66276d6ff06918a3e07fd869003f7527cc39f033
TianhaoranLY <tianhaoran@bjwwtd.com>
2017-05-19 11:11:49 +0800

    QTA-347 已下架判断

--- src/main/java/libraryextra/bean/RemedialClassDetailBean.java
+++ src/main/java/libraryextra/bean/RemedialClassDetailBean.java
@@ -30,44 +30,6 @@ public class RemedialClassDetailBean implements Serializable {
     }
 
     public static class Data {
-        /**
-         * id : 30
-         * name : 初中历史
-         * subject : 历史
-         * grade : 初二
-         * teacher_name : 史晓军
-         * price : 100
-         * current_price : 50
-         * chat_team_id : 25190613
-         * chat_team_owner : c51d5109228be3a04dc2e5e7d37aacb0
-         * buy_tickets_count : 2
-         * status : completed
-         * description : 讲述中国近现代史的基本历程和线索，建立完善的知识网络，强化专题模块，讲练结合，提高归纳和分析能力。
-         * tag_list : []
-         * lesson_count : 2
-         * lessons_count : 2
-         * preset_lesson_count : 2
-         * completed_lesson_count : 2
-         * taste_count : 0
-         * completed_lessons_count : 2
-         * closed_lessons_count : 1
-         * live_start_time : 2016-08-17 10:57
-         * live_end_time : 2016-08-17 12:00
-         * objective : null
-         * suit_crowd : null
-         * publicize : http://qatime-testing.oss-cn-beijing.aliyuncs.com/courses/publicize/app_info_5d2596291925b23459895f063f5958fc.jpg
-         * lessons : [{"id":78,"name":"初中历史","status":"completed","class_date":"2016-08-17","live_time":"10:00-12:00","board_pull_stream":"rtmp://va0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419","camera_pull_stream":null,"replayable":false,"left_replay_times":0},{"id":79,"name":"第一单元  侵略和反抗","status":"missed","class_date":"2016-08-17","live_time":"11:00-12:00","board_pull_stream":"rtmp://va0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419","camera_pull_stream":null,"replayable":false,"left_replay_times":0}]
-         * chat_team : {"announcement":null,"team_announcements":[],"accounts":[{"accid":"c51d5109228be3a04dc2e5e7d37aacb0","name":"寻常巷陌","icon":"http://qatime-testing.oss-cn-beijing.aliyuncs.com/avatars/small_797d14c0c9237ebd866a3284f0789102.jpg"},{"accid":"c44acc93c89dd408a36b2bab4915cb74","name":"怎么不可以重复","icon":"http://qatime-testing.oss-cn-beijing.aliyuncs.com/avatars/small_cde6f9af79f6b0d9bb023759786ee83f.png"}]}
-         * pull_address : rtmp://va0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419
-         * board_pull_stream : rtmp://va0a19f55.live.126.net/live/2794c854398f4d05934157e05e2fe419
-         * camera_pull_stream : null
-         * preview_time : 2016-08-17 11:00
-         * teacher : {"id":406,"name":"史晓军","nick_name":"寻常巷陌","avatar_url":"http://qatime-testing.oss-cn-beijing.aliyuncs.com/avatars/ex_big_797d14c0c9237ebd866a3284f0789102.jpg","ex_big_avatar_url":"http://qatime-testing.oss-cn-beijing.aliyuncs.com/avatars/ex_big_797d14c0c9237ebd866a3284f0789102.jpg","login_mobile":"15333037493","email":"1240366505@qq.com","chat_account":{"user_id":406,"accid":"c51d5109228be3a04dc2e5e7d37aacb0","token":"6104d5a046db568df4447d3019008a94","name":"寻常巷陌","icon":"http://qatime-testing.oss-cn-beijing.aliyuncs.com/avatars/small_797d14c0c9237ebd866a3284f0789102.jpg"},"openid":"o9PoBwc7y_2zUXwfFbPJ5M0f5V9g","teaching_years":"within_three_years","category":"初中","subject":"历史","grade_range":[],"gender":null,"birthday":null,"province":1,"city":1,"school":14,"desc":""}
-         * is_tasting : true
-         * is_bought : false
-         * tasted : false
-         * current_lesson_name : 全部课程已完成
-         */
 
         private int id;
         private String name;
@@ -104,6 +66,15 @@ public class RemedialClassDetailBean implements Serializable {
         private List<String> tag_list;
         private List<Lessons> lessons;
         private Icons icons;
+        private boolean off_shelve;
+
+        public boolean isOff_shelve() {
+            return off_shelve;
+        }
+
+        public void setOff_shelve(boolean off_shelve) {
+            this.off_shelve = off_shelve;
+        }
 
         public Icons getIcons() {
             return icons;
