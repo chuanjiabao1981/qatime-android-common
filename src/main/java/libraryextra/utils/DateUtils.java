862aafe811e5cba73c436b235d83d9124837323a
TianhaoranLY <tianhaoran@bjwwtd.com>
2017-07-31 16:05:35 +0800

    QTA-423 课程表支持专属课、日期格式化

--- src/main/java/libraryextra/utils/DateUtils.java
+++ src/main/java/libraryextra/utils/DateUtils.java
@@ -250,4 +250,22 @@ public class DateUtils {
 
         return Integer.parseInt(String.valueOf(between_days));
     }
+
+    public static int daysBetween(long smdate, long time2) {
+        long time1 = smdate;
+        long between_days = (time1 - time2) / (1000 * 3600 * 24);
+        return Integer.parseInt(String.valueOf(between_days));
+    }
+
+    public static int daysBetween(String smdate, long time2) throws ParseException {
+        if (StringUtils.isNullOrBlanK(smdate)) return 0;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(sdf.parse(smdate));
+        long time1 = cal.getTimeInMillis();
+        long between_days = (time1 - time2) / (1000 * 3600 * 24);
+
+        return Integer.parseInt(String.valueOf(between_days));
+    }
+
 }
