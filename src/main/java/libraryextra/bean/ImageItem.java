6ed643d6d3200d5678dfe7a9b6237026a1800906
TianhaoranLY <tianhaoran@bjwwtd.com>
2017-10-16 16:47:37 +0800

    QTA-444 图片语音支持

--- src/main/java/libraryextra/bean/ImageItem.java
+++ src/main/java/libraryextra/bean/ImageItem.java
@@ -9,4 +9,24 @@ public class ImageItem implements Serializable {
     public String imageId;
     public String thumbnailPath;
     public String imagePath;
+    public Status status = Status.NONE;//图片上传状态
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ImageItem imageItem = (ImageItem) o;
+
+        return imagePath != null ? imagePath.equals(imageItem.imagePath) : imageItem.imagePath == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return imagePath != null ? imagePath.hashCode() : 0;
+    }
+
+    public enum Status {
+        NONE,UPLOADING,SUCCESS,ERROR
+    }
 }
